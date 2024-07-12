6bc3fd0ee25f395a4131a6f1fe1913013d70cfa0
Harold Saldaña Zavaleta <hsaldanaz1@upao.edu.pe>
2024-07-12 16:51:14 -0500

    formulario-mejora en las validaciones

--- src/app/components/formulario-empleado/formulario-empleado.component.ts
+++ src/app/components/formulario-empleado/formulario-empleado.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { EmpleadoService } from '../../service/empleado.service';
 import { SalarioResponse } from '../../models/SalarioResponse';
 
@@ -15,7 +17,9 @@ import { SalarioResponse } from '../../models/SalarioResponse';
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   templateUrl: './formulario-empleado.component.html',
   styleUrls: ['./formulario-empleado.component.css']
@@ -27,7 +31,7 @@ export class FormularioEmpleadoComponent {
   constructor(private fb: FormBuilder, private empleadoService: EmpleadoService) {
     this.empleadoForm = this.fb.group({
       name: ['', Validators.required],
-      hourlyWage: [0, [Validators.required, Validators.min(0)]],
+      hourlyWage: [0, [Validators.required, Validators.min(0.01)]],
       hoursWorked: [0, [Validators.required, Validators.min(0)]],
       overtimeHours: [0, [Validators.required, Validators.min(0)]]
     });
